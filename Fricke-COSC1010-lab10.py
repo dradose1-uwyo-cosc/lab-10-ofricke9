56171b11b474e8d9d709823b5aabde85f11a239d
ofricke9 <oafricke@gmail.com>
2024-11-25 06:12:52 +0000

    submitting lab 10

--- Fricke-COSC1010-lab10.py
+++ Fricke-COSC1010-lab10.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Oliver Fricke
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# 11-21-24
+# Lab 10
+# Lab Section: 13
+# Sources, people worked with, help given to: Help GIVEN TO - Cody Phillips (roommate)
 # your
 # comments
 # here
@@ -17,8 +17,6 @@ def get_hash(to_hash):
     """You can use """
     return sha256(to_hash.encode('utf-8')).hexdigest().upper()
 
-
-
 # Files and Exceptions
 
 # For this assignment, you will be writing a program to "crack" a password. You will need to open the file `hash` as this is the password you are trying to "crack."
@@ -30,16 +28,35 @@ def get_hash(to_hash):
 #   - You can use the provided `get_hash()` function to generate the hashes.
 #   - Be careful, as "hello" and "hello " would generate a different hash.
 
-# You will need to include a try-except-catch block in your code.
+# You will need to include a try-except-else block in your code.
 # - The reading of files needs to occur in the try blocks.
 
+p = Path('hash')
+dec = Path('rockyou.txt')
 
 # - Read in the value stored within `hash`.
 #   - You must use a try and except block.
 
+try:
+    crack = p.read_text().strip()
+except FileNotFoundError:
+    print("Error")
 
 # Read in the passwords in `rockyou.txt`.
 # - Again, you need a try-except-else block.
 # Hash each individual password and compare it against the stored hash.
 # - When you find the match, print the plaintext version of the password.
 # - End your loop.
+
+try:
+    password = dec.read_text().splitlines()
+except FileNotFoundError:
+    print("Error")
+
+try:
+    for line in password:
+        if get_hash(line) == crack:
+            print(f"The name of the password is: {line}")
+            print(f"The 256-bit of the password is: {crack}")
+except ValueError:
+    print("Error")
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
+# [Oliver Fricke]
+## [Assignment - Lab 10]
+## [Submission date: 11/24/24]
 ## Worked with/sources 
-* example
+* Helped Cody Phillips (my roommate)
 * example
 * example
 * example
 ## Project Quirks/ Things that don't work
-* example
+* Struggled at first to work with the long file, but used splitlines(), and quickly resolved it. Fairly straightforward after. 
 * example
 * example
